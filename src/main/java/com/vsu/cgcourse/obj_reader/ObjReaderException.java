205707a59c41f96c150d06445d7133ea664e90d0
tukitoki <tokichiihere@gmail.com>
2021-11-19 17:18:49 +0300

    main project

---
+++ src/main/java/com/vsu/cgcourse/obj_reader/ObjReaderException.java
@@ -0,0 +1,7 @@
+package com.vsu.cgcourse.obj_reader;
+
+class ObjReaderException extends RuntimeException {
+    public ObjReaderException(String errorMessage, int lineInd) {
+        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
+    }
+}
