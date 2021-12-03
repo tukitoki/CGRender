053dd272b0faafc98ff6ea474896b0c9b51276cf
Dunad4n <sosiska7150@gmail.com>
2021-12-03 19:14:09 +0300

    sosat' x2

--- src/main/test/com/vsu/cgcourse/math/AbstractVectorTest.java
+++ src/main/test/com/vsu/cgcourse/math/AbstractVectorTest.java
@@ -1,7 +1,5 @@
 package com.vsu.cgcourse.math;
 
-import com.vsu.cgcourse.math.Matrix4;
-import com.vsu.cgcourse.math.Vector4;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -23,19 +21,22 @@ class AbstractVectorTest {
 
     @Test
     public void multiplyMatrixByMatrix() throws Exception {
-        Matrix3 matrix3 = new Matrix3(new float[][] {{5, 2, 7},
-                                                     {4, 8, 9},
-                                                    {12, 15, 2}});
-        Matrix3 matrix31 = new Matrix3(new float[][] {{4, 7, 2},
-                                                      {8, 1, 2},
-                                                      {9, 15, 11}});
-        matrix3.multiply(matrix31);
-        Matrix3 matrix32 = new Matrix3(new float[][] {{99, 142, 91},
-                                                      {161, 171, 123},
-                                                      {186, 129, 76}});
-        for (int i = 0; i < matrix3.getMatrix().length; i++) {
-            for (int j = 0; j < matrix3.getMatrix()[i].length; j++) {
-                Assertions.assertEquals(matrix3.getMatrix()[i][j], matrix32.getMatrix()[i][j]);
+        Matrix4 matrix4 = new Matrix4(new float[][] {{5, 2, 7, 8},
+                                                     {4, 8, 9, 8},
+                                                    {12, 15, 2, 8},
+                                                     {4, 9, 9, 8}});
+        Matrix4 matrix41 = new Matrix4(new float[][] {{4, 7, 2, 8},
+                                                      {8, 1, 2, 8},
+                                                      {9, 15, 11, 8},
+                                                      {5, 4, 4, 2}});
+        matrix4.multiply(matrix41);
+        Matrix4 matrix42 = new Matrix4(new float[][] {{139, 174, 123, 128},
+                                                      {201 ,203, 155, 184},
+                                                      {226, 161, 108, 248},
+                                                      {209, 204, 157, 192}});
+        for (int i = 0; i < matrix4.getMatrix().length; i++) {
+            for (int j = 0; j < matrix4.getMatrix()[i].length; j++) {
+                Assertions.assertEquals(matrix4.getMatrix()[i][j], matrix42.getMatrix()[i][j]);
             }
         }
     }
